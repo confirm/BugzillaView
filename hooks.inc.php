266afed8c4a5fd76014d22327da3607b7db31874
Dominique Barton <github@barton-online.ch>
2013-11-27 14:52:17 +0100

    Better exception handling in case something w/ the Bugzilla DB connection is going wrong.

--- hooks.inc.php
+++ hooks.inc.php
@@ -36,39 +36,46 @@ function bugzillaViewLinkEnd($skin, $target, $options, &$html, &$attribs, &$ret)
     $text = ($html != $wgBugzillaView['interwiki'].':'.$id) ? $html : '';
     $classes = 'bug';
 
-    // get infos from bugzilla DB
-    $statement = BugzillaViewDatabase::getInstance()->query('SELECT resolution, short_desc, bug_status
-        FROM '.$wgBugzillaView['dbTablePrefix'].'bugs
-        WHERE bug_id = "'.$id.'"
-        LIMIT 1');
-
-    // get data record from database
-    $bug = $statement->fetch(PDO::FETCH_OBJ);
-
-    // handle correct bug IDs
-    if(is_object($bug))
+    try
     {
-        if(strlen(trim($text)) == 0)
+        // get infos from bugzilla DB
+        $statement = BugzillaViewDatabase::getInstance()->query('SELECT resolution, short_desc, bug_status
+            FROM '.$wgBugzillaView['dbTablePrefix'].'bugs
+            WHERE bug_id = "'.$id.'"
+            LIMIT 1');
+
+        // get data record from database
+        $bug = $statement->fetch(PDO::FETCH_OBJ);
+
+        // handle correct bug IDs
+        if(is_object($bug))
         {
-            $text  = $id.' ';
-            if($wgBugzillaView['displayDescription'])
-                $text .= '<span class="bug-label-description">'.utf8_encode($bug->short_desc).'</span> ';
-            $text .= '<span class="bug-label-status">'.$bug->bug_status.'</span> ';
-            $text .= '<span class="bug-label-resolution">'.$bug->resolution.'</span>';
+            if(strlen(trim($text)) == 0)
+            {
+                $text  = $id.' ';
+                if($wgBugzillaView['displayDescription'])
+                    $text .= '<span class="bug-label-description">'.utf8_encode($bug->short_desc).'</span> ';
+                $text .= '<span class="bug-label-status">'.$bug->bug_status.'</span> ';
+                $text .= '<span class="bug-label-resolution">'.$bug->resolution.'</span>';
+            }
+
+            $classes .= ' bug-status-'.strtolower($bug->bug_status).' bug-resolution-'.($bug->resolution ? strtolower($bug->resolution) : 'unknown');
         }
 
-        $classes .= ' bug-status-'.strtolower($bug->bug_status).' bug-resolution-'.($bug->resolution ? strtolower($bug->resolution) : 'unknown');
-    }
+        // handle incorrect bug IDs
+        else
+        {
+            $text    = 'Invalid Bug #'.$id;
+            $classes = ' bug-status-notfound';
+        }
 
-    // handle incorrect bug IDs
-    else
+        // build link (image sponsored by http://www.fatcow.com/free-icons)
+        $ret = Xml::openElement('a', $attribs).'<span class="bug '.$classes.'"><img src="'.$wgScriptPath.'/extensions/BugzillaView/bug.png"> '.$text.'</span>'.Xml::closeElement('a');
+    }
+    catch(Exception $e)
     {
-        $text    = 'Invalid Bug #'.$id;
-        $classes = ' bug-status-notfound';
+        echo 'BugzillaView extension error: '.$e->getMessage();
     }
-
-    // build link (image sponsored by http://www.fatcow.com/free-icons)
-    $ret = Xml::openElement('a', $attribs).'<span class="bug '.$classes.'"><img src="'.$wgScriptPath.'/extensions/BugzillaView/bug.png"> '.$text.'</span>'.Xml::closeElement('a');
 
     // tell MW to skip any further hooks
     return FALSE;
