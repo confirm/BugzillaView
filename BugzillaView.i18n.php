002b05c93e2757ba5bff33722b0868e730191923
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-01-24 14:00:59 +0100

    Create BugzillaView.i18n.php
    Add I18n-support

---
+++ BugzillaView.i18n.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @brief Internationalisation file for the BugzillaView extension
+ * @file BugzillaView/BugzillaView.i18n.php
+ *
+ * @author Karsten Hoffmeyer (Kghbln)
+ */
+
+$messages = array();
+
+/** English
+ * @author Dbarton
+ * @author Kghbln
+ */
+$messages['en'] = array(
+        'bugzillaview-desc' => 'Allows to display BugZilla bugs on pages'
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+        'bugzillaview-desc' => 'Ermöglicht die Darstellung von Programmfehlern aus BugZilla auf Seiten'
+);
