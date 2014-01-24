08e3068cdcd7df6a5bd0faa8893c16d8a8d62f41
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-01-24 13:47:37 +0100

    Update BugzillaView.php
    * Increment version to 1.0.1
    * Improve file documentation
    * Autoload class file
    * Add I18n-support
    * Remove PHP closing tag
    * Formatting

--- BugzillaView.php
+++ BugzillaView.php
@@ -1,36 +1,45 @@
 <?php
 /**
- * @file    BugzillaView/BugzillaView.php
- *
- * @brief   Main script of BugzillaView which defines all hooks.
- *
- * @author  dbarton, confirm IT solutions GmbH
- */
+* @brief Initialization file for the BugzillaView extension.
+* @file BugzillaView/BugzillaView.php
+* @ingroup Extensions
+* 
+* @version 1.0.1 - 2014-01-24
+*
+* @link https://www.mediawiki.org/wiki/Extension:BugzillaView Documentation
+* @link https://www.mediawiki.org/wiki/Extension_talk:BugzillaView Support
+* @link https://github.com/domibarton/BugzillaView Source Code
+*
+* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+* @author Dominique Barton (dbarton), confirm IT solutions GmbH
+*/
 
-/*
+/**
  * INITIALIZATION
  */
 
 // abort if this script is called directly
-if (!defined('MEDIAWIKI')) die("MediaWiki extensions cannot be run directly.");
+if( !defined('MEDIAWIKI' ) ) {
+        die( 'This file is a MediaWiki extension. It is not a valid entry point' );
+}
 
 // extension credits
 $wgExtensionCredits['other'][] = array(
-    'path'        => __FILE__,
-    'name'        => 'BugzillaView',
-    'version'     => '1.0',
-    'author'      => 'dbarton, confirm IT solutions',
-    'description' => 'MediaWiki extension to display Bugzilla bug details'
+    'path'           => __FILE__,
+    'name'           => 'BugzillaView',
+    'version'        => '1.0.1',
+    'author'         => 'Dominique Barton',
+    'descriptionmsg' => 'bugzillaview-desc'
 );
 
 // load required files
 require 'hooks.inc.php';
-require 'BugzillaViewDatabase.class.php';
+$wgAutoloadClasses['BugzillaViewDatabase'] = __DIR__ . '/BugzillaViewDatabase.class.php';
+$wgExtensionMessagesFiles['BugzillaView'] = __DIR__ . '/BugzillaView.i18n.php';
 
-/*
+/**
  * DEFAULT SETTINGS
  */
-
 $wgBugzillaView = array(
     'interwiki'          => 'bugzilla',
     'displayDescription' => TRUE,
@@ -42,11 +51,8 @@ $wgBugzillaView = array(
     'dbTablePrefix'      => ''
 );
 
-/*
+/**
  * REGISTER HOOKS
  */
-
 $wgHooks['LinkEnd'][]           = 'bugzillaViewLinkEnd';
 $wgHooks['BeforePageDisplay'][] = 'bugzillaViewBeforePageDisplay';
-
-?>
